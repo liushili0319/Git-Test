be68ccb4070719ce8a3ebf3c7ca546fa98e7b622
liusir <liushili0319@163.com>
2021-12-13 18:53:34 +0800

    two commit

--- Git-Test/src/main/java/com/liu/Git/Git.java
+++ Git-Test/src/main/java/com/liu/Git/Git.java
@@ -3,5 +3,6 @@ package com.liu.Git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello GIt!");
+        System.out.println("Hello GIt2!");
     }
 }
