9ceb61e2506ce6aeebcd071ab9f9fa9d253e944f
liusir <liushili0319@163.com>
2021-12-13 20:25:19 +0800

    java8 commit

--- Git-Test/src/main/java/com/liu/Git/Git.java
+++ Git-Test/src/main/java/com/liu/Git/Git.java
@@ -1,9 +1,15 @@
 package com.liu.Git;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello GIt!");
         System.out.println("Hello GIt2!");
         System.out.println("Hello GIt3!");
+        Integer[] arr={1,5,3,2};
+        Arrays.sort(arr, (o1, o2) -> Integer.compare(o1,o2)); // lambda 表达式
     }
 }
